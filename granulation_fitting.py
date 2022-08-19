280eb7565a2da05d0cdd25c46e0f5b4fe5a78a72
nielsenmb <martinbonielsen.astro@gmail.com>
2022-08-19 11:01:52 +0100

    adding DR and nonDR options

--- granulation_fitting.py
+++ granulation_fitting.py
@@ -2,28 +2,28 @@ import jax.numpy as jnp
 from functools import partial
 import jax, dynesty, corner, os
 from dynesty import utils as dyfunc
-import utils as dist
 import numpy as np
 from IO import psd
 from utils import scalingRelations
+from DR import PCA
+import utils
 jax.config.update('jax_enable_x64', True)
-
 
 
 class granulation_fit(scalingRelations):
  
-    def __init__(self, ID, numax, download_dir):
+    def __init__(self, ID, numax, download_dir, pcadim=None, fname='PCAsample.csv'):
 
         self.ID = ID
 
         self.numax_guess = numax
 
+        self.log_numax_guess = utils.to_log10(*self.numax_guess)
+
         self.download_dir = download_dir
 
-        print('Init psd class')
         self.psd = psd(self.ID, numaxGuess=self.numax_guess, downloadDir=download_dir)
 
-        print('Computing psd')
         self.psd()
         
         self.f, self.p = self.psd.freq[self.psd.freq > 1], self.psd.powerdensity[self.psd.freq > 1]
@@ -34,19 +34,42 @@ class granulation_fit(scalingRelations):
 
         self.Nyquist = self.psd.Nyquist * 1e6
 
-        self.ndim = 13
-        
         self.eta = self.eta()
 
-        self.addPriors()
-
         # Envelope width conversion factor
-        self.wfac = 1/ (2 * jnp.sqrt(2 * jnp.log(2))) / 2
+        self.wfac = 1 / (2 * jnp.sqrt(2 * jnp.log(2))) / 2
 
         if self.psd.pbins > 1:
             self._lnlike = self._lnlike_binned
         else:
             self._lnlike = self._lnlike_std
+
+        self.labels = ['bkg_numax', 'bkg_envHeight', 'bkg_envWidth',
+                       'H1_power', 'H1_nu', 'H1_exp',
+                       'H2_power', 'H2_nu', 'H2_exp',
+                       'H3_power', 'H3_nu', 'H3_exp',
+                       'white']
+
+        if pcadim is not None:
+            self.with_pca = True
+
+        else:
+            self.with_pca = False
+
+        if self.with_pca:
+            
+            self.pcalabels = ['bkg_numax', 'bkg_envHeight', 'bkg_envWidth',
+                              'H1_power', 'H1_nu', 'H1_exp',
+                              'H2_power', 'H2_nu', 'H2_exp',]
+
+            self.ndim = pcadim + (len(self.labels) - len(self.pcalabels)) 
+
+            self.initPCA(pcadim)
+
+        else:
+            self.ndim = len(self.labels)
+
+        self.addPriors()
 
     def addPriors(self):
         """Add a list of priors as an attribute to self
@@ -60,38 +83,48 @@ class granulation_fit(scalingRelations):
 
         self.priors = []
 
+        # Add the 3rd Harvey law and white noise
         widx = self.f > self.Nyquist - 10
 
-        pw = self.p[widx].mean()
+        pw = np.nanmean(self.p[widx])
 
-        # Harvey 1 (envelope harvey)
-        self.addHarveyPriors(self.nuHarveyEnv(self.numax_guess[0]), pw)
+        if self.with_pca:
+            # Add pca parameters to the list of prior objects
+            for i in range(self.DR.dims_R):
+                self.priors.append(utils.distribution(self.DR.pdf[i], self.DR.ppf[i]))
+        else:
+             # numax
+            self.priors.append(utils.normal(mu=self.numax_guess[0], 
+                                            sigma=self.numax_guess[1]))
 
-        # Harvey 2 (granulation)
-        self.addHarveyPriors(self.nuHarveyGran(self.numax_guess[0]), pw)
+            # Envelope dwidth
+            self.priors.append(utils.beta(a=1.2, b=1.2, loc=0.2, scale=2))
+
+            # Envelope height
+            numax_idx = (abs(self.f - self.numax_guess[0]) < self.envWidth(self.numax_guess[0]))
+            self.priors.append(utils.normal(mu=jnp.log10(self.p[numax_idx].mean()), sigma=2))
+
+             # Harvey 1 (envelope harvey)
+            self.addHarveyPriors(self.nuHarveyEnv(self.numax_guess[0]), pw)
+
+            # Harvey 2 (granulation)
+            self.addHarveyPriors(self.nuHarveyGran(self.numax_guess[0]), pw)
 
         # Harvey 3 Activity
         self.addHarveyPriors(self.f[0], pw)
 
-        # numax
-        self.priors.append(dist.normal(mu=self.numax_guess[0], 
-                                       sigma=self.numax_guess[1]))
-
-        # Envelope dwidth
-        self.priors.append(dist.beta(a=1.2, b=1.2, loc=0.2, scale=2))
-
-        # Envelope height
-        numax_idx = (abs(self.f - self.numax_guess[0]) < self.envWidth(self.numax_guess[0]))
-        self.priors.append(dist.normal(mu=jnp.log10(self.p[numax_idx].mean()), sigma=2))
-
         # White noise
-        self.priors.append(dist.normal(mu=jnp.log10(pw), sigma=2))
+        self.priors.append(utils.normal(mu=jnp.log10(pw), sigma=2))
+ 
+    def initPCA(self, PCAdim):
 
-        self.labels = ['hsig1', 'dhnu1', 'hexp1',
-                       'hsig2', 'dhnu2', 'hexp2',
-                       'hsig3', 'hnu3', 'hexp3',
-                       'numax', 'dwidth', 'height',
-                       'white']
+        self.DR = PCA(self.log_numax_guess, self.pcalabels)
+
+        self.DR.fit_weightedPCA(PCAdim)
+
+        _Y = self.DR.transform(self.DR.data_F)
+
+        self.DR.ppf, self.DR.pdf = self.DR.getQuantileFuncs(_Y)
 
     def addHarveyPriors(self, nuH, pw):
         """ Append Harvey law prior
@@ -126,11 +159,11 @@ class granulation_fit(scalingRelations):
 
         mu = jnp.array([1, self.p[idx].mean() - pw]).max()
 
-        self.priors.append(dist.normal(mu=jnp.log10(mu*nuH), sigma=1)) # hsig
+        self.priors.append(utils.normal(mu=jnp.log10(mu*nuH), sigma=2)) # hsig
 
-        self.priors.append(dist.beta(a=1.2, b=1.2, loc=0.2, scale=3)) # dhnu
+        self.priors.append(utils.beta(a=1.2, b=1.2, loc=0.2, scale=6)) # hnu
 
-        self.priors.append(dist.beta(a=1.2, b=1.2, loc=1, scale=5)) # hexp
+        self.priors.append(utils.beta(a=1.2, b=1.2, loc=0.5, scale=5)) # hexp
     
     def eta(self,):
         return jnp.sinc(self._f / 2.0 / self.Nyquist)**2.0    
@@ -152,7 +185,9 @@ class granulation_fit(scalingRelations):
             list u.
         """
 
-        x = [self.priors[n].ppf(u[n]) for n in range(self.ndim)]
+        #x_DR = jnp.array([self.DR.ppf[i](u[i]) for i in range(self.DR.dims_R)])
+ 
+        x = jnp.array([self.priors[i].ppf(u[i]) for i in range(self.ndim)])
 
         return x
 
@@ -164,7 +199,7 @@ class granulation_fit(scalingRelations):
 
         """
 
-        har = 10**hsig / hnu / (1.0 + (self._f / hnu)**hexp)
+        har = hsig / hnu / (1.0 + (self._f / hnu)**hexp)
 
         return har
 
@@ -176,7 +211,7 @@ class granulation_fit(scalingRelations):
 
         """
 
-        m = 10**height * jnp.exp(-(self._f - numax)**2 / (2.0 * width**2))
+        m = height * jnp.exp(-(self._f - numax)**2 / (2.0 * width**2))
 
         return m
 
@@ -203,29 +238,88 @@ class granulation_fit(scalingRelations):
             Dictionary of each model term.
         """
         
-        hsig1, dhnu1, exp1, hsig2, dhnu2, exp2, hsig3, hnu3, exp3, numax, dwidth, height, white = params
-
+        numax, height, width, hsig1, hnu1, exp1, hsig2, hnu2, exp2, hsig3, hnu3, exp3, W = self.unpackParams(params)
+        
         # Start with Harvey laws
-        hnu1 = self.nuHarveyEnv(numax) * dhnu1
         H1 = self.harvey(hsig1, hnu1, exp1)
 
-        hnu2 = self.nuHarveyGran(numax) * dhnu2
         H2 = self.harvey(hsig2, hnu2, exp2)
 
         H3 = self.harvey(hsig3, hnu3, exp3)
 
         # Add envelope
-        width = dwidth * self.envWidth(numax) * self.wfac
         G = self.gaussian(numax, width, height)
 
-        # Add white noise
-        W = 10**white
-
-        _model = (H1 + H2 + H3 + G)*self.eta 
+        _model = (H1 + H2 + H3 + G) * self.eta 
         
-        model = _model[:len(self.f)]+ W + _model[len(self.f):][::-1] 
+        model = _model[:len(self.f)] + W + _model[len(self.f):][::-1] 
 
-        return model, _model, {'H1': H1, 'H2': H2, 'H3': H3, 'G': G, 'W': W}
+        comps = {'H1': H1, 'H2': H2, 'H3': H3, 'G': G, 'W': W}
+
+        extra = {'hnu1': hnu1, 'hnu2': hnu2, 'hnu3': hnu3, 'numax': numax }
+
+        return model, _model, comps, extra 
+
+    @partial(jax.jit, static_argnums=(0,))
+    def unpackParams(self, theta): 
+        """ Separate the model parameters into asy, bkg, reg and phi
+
+        This will need to be updated when bkg is rolled into DR.
+
+        Parameters
+        ----------
+        theta : array
+            Array of parameters drawn from the posterior distribution.
+
+        Returns
+        -------
+        theta_asy : array
+            The unpacked parameters of the p-mode asymptotic relation.
+        theta_obs : array
+            The unpacked parameters of the observational parameters.
+        theta_bkg : array
+            The unpacked parameters of the harvey background model.
+        theta_reg : array
+            The unpacked parameters of the reggae model.
+        phi : array
+            The model scaling parameter.             
+        """
+        if self.with_pca:
+            # The sample used for DR is in log10, so the inverse transform 
+            theta_invT = 10**self.DR.inverse_transform(theta[:self.DR.dims_R])
+            
+            numax, height, width  = theta_invT[0:3] 
+
+            hsig1, hnu1, exp1 = theta_invT[3:6] 
+    
+            hsig2, hnu2, exp2 = theta_invT[6:9]
+
+            hsig3, hnu3, exp3, white = theta[self.DR.dims_R:]
+
+            hsig3 = 10**hsig3
+
+            w = 10**white
+
+        else:
+            numax, dwidth, height, hsig1, dhnu1, exp1, hsig2, dhnu2, exp2, hsig3, hnu3, exp3, white = theta
+
+            height = 10**height
+
+            # Start with Harvey laws
+            hsig1 = 10**hsig1
+            hnu1 = self.nuHarveyEnv(numax) * dhnu1
+            
+            hsig2 = 10**hsig2
+            hnu2 = self.nuHarveyGran(numax) * dhnu2
+        
+            # Add envelope
+            width = dwidth * self.envWidth(numax) * self.wfac
+            
+            hsig3 = 10**hsig3
+
+            w = 10**white
+
+        return numax, height, width, hsig1, hnu1, exp1, hsig2, hnu2, exp2, hsig3, hnu3, exp3, w
 
     @partial(jax.jit, static_argnums=(0,))
     def lnfactorial(self, n):
@@ -263,20 +357,13 @@ class granulation_fit(scalingRelations):
     @partial(jax.jit, static_argnums=(0,))
     def lnlike(self, params):
 
-        mod, _mod, _ = self.model(params)
+        mod, _mod, _, extra= self.model(params)
+ 
+        T = (extra['hnu3'] < extra['hnu2']) & \
+            (extra['hnu2'] < extra['numax']) & \
+            (extra['hnu1']/extra['hnu2'] > 1.1)
 
-        hsig1, dhnu1, exp1, hsig2, dhnu2, exp2, hsig3, hnu3, exp3, numax, dwidth, height, white = params
-
-        hnu1 = self.nuHarveyEnv(numax) * dhnu1
-
-        hnu2 = self.nuHarveyGran(numax) * dhnu2
-
-        T = (hnu3 > hnu2) & \
-            (hnu2 > hnu1) & \
-            (hnu2 > numax) & \
-            (hnu1/hnu2 < 0.1)
-
-        L = jax.lax.cond(T, lambda mod: -jnp.inf, self._lnlike, mod)
+        L = jax.lax.cond(T, self._lnlike, lambda mod: -jnp.inf, mod)
 
         return L
 
@@ -284,7 +371,7 @@ class granulation_fit(scalingRelations):
 
         sampler = dynesty.NestedSampler(self.lnlike, self.ptform, self.ndim, nlive=nlive)
         
-        sampler.run_nested(print_progress=False)   
+        sampler.run_nested(print_progress=True)   
 
         result = sampler.results
 
@@ -294,41 +381,58 @@ class granulation_fit(scalingRelations):
 
         return sampler, new_samples
 
-    def makeCorner(self, fig, samples, N, labels=None, outputDir=None):
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def makePriorCorner(self, fig, N = 500):
+
+        prior_samples = np.array([self.ptform(np.random.uniform(low=0, high=1, size=self.ndim)) for i in range(N)])
+
+        self.makeSamplingCorner(fig, prior_samples)
+
+    def makeSamplingCorner(self, fig, samples, N=500, outputDir=None):
+        
         thinIdx = np.random.choice(np.arange(samples.shape[0]), size=min([N, samples.shape[0]]), replace=False)
+        
+        if self.with_pca:
+            labels = [r'$\theta_%i$' % (i) for i in range(self.DR.dims_R)] + [x for x in self.labels if x not in self.pcalabels]
+        else:
+            labels = self.labels
 
-        ndim = len(labels)
+        corner.corner(samples[thinIdx, :], labels=labels, hist_kwargs={'density': True}, fig=fig);
+        
+        axes = np.array(fig.axes).reshape((self.ndim, self.ndim))
+        
+        for i in range(self.ndim):
 
-        idx = np.sum(np.array([x == np.array(self.labels) for x in labels]), axis = 0).astype(bool)
-
-        corner.corner(samples[thinIdx, :][:, idx], labels=labels, hist_kwargs={'density': True}, fig=fig);
-
-        pr = np.array(self.priors)[idx]
-
-        axes = np.array(fig.axes).reshape((ndim, ndim))
-
-        for i in range(ndim):
-            xl = jnp.linspace(pr[i].ppf(0.001), pr[i].ppf(0.5), 101)
-
-            xu = jnp.linspace(pr[i].ppf(0.5), pr[i].ppf(0.999), 101)
-
-            axes[i,i].plot(xl, pr[i].pdf(xl), color = 'C0', lw=4)
-
-            axes[i,i].plot(xu, pr[i].pdf(xu), color = 'C1', lw=4)
-
-            axes[i,i].axvline(pr[i].ppf(0.5), color = 'k', ls='dashed')
-
-        if not outputDir is None:
-            if len(labels) == self.ndim:
-                fname = '_all.png'
-
-            else:
-                fname = '_'+'_'.join(labels)+'.png'
-
-            path = os.path.join(*[outputDir, os.path.basename(outputDir) + fname])
-
-            fig.savefig(path)
+            utils._priorCurve(axes[i,i], self.priors[i].ppf, self.priors[i].pdf)
+ 
 
     def plotModel(self, fig, ax, samples=None, outputDir=None):
 
@@ -337,7 +441,7 @@ class granulation_fit(scalingRelations):
             if idx is None:
                 idx = np.arange(len(self.f))
 
-            model, _model, comps = self.model(params)
+            model, _model, comps, _ = self.model(params)
 
             ax.plot(self.f[idx], model[idx], lw =5, color = 'k', alpha=alpha)
 
@@ -363,7 +467,7 @@ class granulation_fit(scalingRelations):
         else:
             nsamples = len(samples[:, 0])
 
-            thinIdx = gen_log_space(len(self.f), 300)
+            thinIdx = utils.gen_log_space(len(self.f), 300)
 
             rnd = np.random.choice(np.arange(nsamples), size=30, replace=False)
 
@@ -397,26 +501,35 @@ class granulation_fit(scalingRelations):
 
         np.savez_compressed(path, samples=samples)
 
-def gen_log_space(limit, n):
+    def makeFullCorner(self, fig, samples, N=500, outputDir=None):
 
-    result = [1]
+        axes = np.array(fig.axes).reshape((len(self.labels), len(self.labels)))
+        
+        thinIdx = np.random.choice(np.arange(samples.shape[0]), 
+                                size=min([N, samples.shape[0]]), 
+                                replace=False)
 
-    if n>1:  # just a check to avoid ZeroDivisionError
-        ratio = (float(limit)/result[-1]) ** (1.0/(n-len(result)))
+        full_samples = np.zeros((N, len(self.labels)))
+        
+        for i, sample in enumerate(samples[thinIdx, :]):
+            
+            full_samples[i, :] = self.unpackParams(sample)
+        
+        # Convert some values back to log
+        for i in list(range(10))+[12]:
+            full_samples[:, i] = np.log10(full_samples[:, i])
 
-    while len(result)<n:
-        next_value = result[-1]*ratio
+        corner.corner(full_samples, labels=self.labels, hist_kwargs={'density': True}, fig=fig);
+        
+        Fti = self.DR.inverse_transform(self.DR.data_R)
+        
+        Fti_ppfs, Fti_pdfs = self.DR.getQuantileFuncs(Fti)
+        
+        for i in range(self.DR.dims_F):
+            
+            utils._priorCurve(axes[i, i], Fti_ppfs[i], Fti_pdfs[i])
 
-        if next_value - result[-1] >= 1:
-            # safe zone. next_value will be a different integer
-            result.append(next_value)
+        for i, j in enumerate(range(self.DR.dims_R, self.DR.dims_R + len(self.labels) - self.DR.dims_F)):
+            
+            utils._priorCurve(axes[i + self.DR.dims_F, i + self.DR.dims_F], self.priors[j].ppf, self.priors[j].pdf)
 
-        else:
-            # problem! same integer. we need to find next_value by artificially incrementing previous value
-            result.append(result[-1]+1)
-
-            # recalculate the ratio so that the remaining values will scale correctly
-            ratio = (float(limit)/result[-1]) ** (1.0/(n-len(result)))
-
-    # round, re-adjust to 0 indexing (i.e. minus 1) and return np.uint64 array
-    return np.array(list(map(lambda x: round(x)-1, result)), dtype=np.uint64)
