783906f3359577fe107ae8b5b3eb197429147fac
nielsenmb <martinbonielsen.astro@gmail.com>
2022-10-26 16:46:02 +0100

    turn progress off

--- BBscript_subset.py
+++ BBscript_subset.py
@@ -34,7 +34,7 @@ for i in idxs:
     _numax = prior_data.loc[i, 'numax']
     _teff = prior_data.loc[i, 'teff']
     _bp_rp = prior_data.loc[i, 'bp_rp'] 
-    
+
 
     obs = {'numax': [10**_numax, 0.01*10**_numax], 
            'teff': [10**_teff, 100],
@@ -42,6 +42,6 @@ for i in idxs:
 
     sfit = spectrum_fit(ID, obs, download_dir, pcadim=pcadim, N=200, fname=prior_data_fname)
         
-    dynSampler, dynSamples = sfit.runDynesty(progress=True)
+    dynSampler, dynSamples = sfit.runDynesty(progress=False)
 
     sfit.storeResults(outputDir)
